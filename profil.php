f6595a3b6deebc63f1afe642c4635cb604e5c910
rymtouzri-debug <rym.touzri@edu.isetcom.tn>
2026-03-30 16:19:27 +0100

    Add files via upload

---
+++ profil.php
@@ -0,0 +1,19 @@
+<?php
+$nom = "Touzri";
+$prenom = "Rym";
+$email = "rym.touzri@email.com";
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="UTF-8">
+<title>Profil utilisateur</title>
+</head>
+<body>
+<h1>Profil utilisateur</h1>
+<p><strong>Nom :</strong> <?= $nom ?></p>
+<p><strong>Prénom :</strong> <?= $prenom ?></p>
+<p><strong>Email :</strong> <?= $email ?></p>
+</body>
+</html>
+<p>Date : <?= date("H:i:s") ?></p>
